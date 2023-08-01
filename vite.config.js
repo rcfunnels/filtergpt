c8efb1c1aa6b4ad4413dab6409fdb1790b6ed1b9
Chetan Alla <allachetan@yahoo.com>
2023-08-01 12:57:24 -0400

    add: deploy workflow

--- vite.config.js
+++ vite.config.js
@@ -6,4 +6,5 @@ import pluginRewriteAll from 'vite-plugin-rewrite-all';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), pluginRewriteAll()],
+  base: "/filtergpt/"
 })
