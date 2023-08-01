10b61fe1bf5a7254c92d4092822ac55b75e9a085
Chetan Alla <allachetan@yahoo.com>
2023-08-01 14:06:40 -0400

    Fixed routing bugs

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,6 @@ import pluginRewriteAll from 'vite-plugin-rewrite-all';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), pluginRewriteAll()]
+  plugins: [react(), pluginRewriteAll()],
+  base: "/filtergpt/"
 })
